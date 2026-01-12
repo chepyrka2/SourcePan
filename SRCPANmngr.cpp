a0f5420c380daabe354122c3f5a5226cd4467933
AlexanderG <REDACTED>
2026-01-12 09:30:53 +0300

    during work/started doing the extractZip()

--- SRCPANmngr.cpp
+++ SRCPANmngr.cpp
@@ -1,25 +1,42 @@
 #include <filesystem>
+#include <iostream>
 #include <zip.h>
+#include <cstdlib>
 #include <string>
 #include "SRCPANmngr.hpp"
 #include "Recipe.hpp"
 
 namespace fs = std::filesystem;
 
-void StartSRCPAN(){
-  if(!fs::exists(fs::path("temprecs"))){
-    fs::create_directory(fs::path("temprecs"));
-  }
+char getOS(){
+  #if defined(_WIN32) || defined(_WIN64)
+    return 'w';
+  #elif defined(__linux__)
+    return 'l';
+  #else
+    std::exit(67);
+  #endif
+
 }
 
-void QuitSRCPAN(){
-  if(fs::exists(fs::path("temprecs"))){
-    fs::remove_all(fs::path("temprecs"));
+fs::path homedir(){
+  if(getOS() == 'w') return fs::path(std::getenv("USERPROFILE"));
+  return std::getenv("HOME");
+}
+
+void StartSRCPAN(){
+  if(!fs::exists(fs::path("recs"))){
+    fs::create_directory(fs::path("recs"));
   }
+  if(!fs::exists(fs::path("recs"))) StartSRCPAN();
 }
 
 Recipe unpack(fs::path pth){
-  if((!fs::exists(pth)) || ((pth.extension() != ".srcpan") && (pth.extension() == ".zip"))) return placeholdersalad;
+  if((!fs::exists(pth)) || ((pth.extension() != ".srcpan") && (pth.extension() != ".zip"))) return placeholdersalad;
+  if(!fs::exists(fs::path("recs"))) StartSRCPAN();
+  fs::path out = fs::path("recs/"+pth.filename().string());
+  fs::copy_file(pth, out);
+  fs::rename(out, out.replace_extension(".zip"));
   
-
 }
+
