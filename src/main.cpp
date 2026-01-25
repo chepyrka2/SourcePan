e27e4a19d79ddde06b06448b4b8a5d00340b25e2
AlexanderG <REDACTED>
2026-01-25 21:00:45 +0300

    resize()

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,29 @@
 #include <string>
 #include <cctype>
 #include "headers/SRCPANmngr.hpp"
+#include <filesystem>
 #include "headers/Recipe.hpp"
 #include "headers/Levenshtein.hpp"
+
+namespace fs = std::filesystem;
 
 const int w = 1280;
 const int h = 720;
 const char title[] = "SourcePan";
 const int fps = 60;
+
+Texture2D resize(fs::path img, int x, int y) {
+  if (!fs::exists(img)) {
+    std::cerr << "Image not found" << std::endl;
+    exit(1);
+  }
+  Image image = LoadImage(img.c_str());
+  ImageResize(&image, x, y);
+  Texture2D out = LoadTextureFromImage(image);
+  SetTextureFilter(out, TEXTURE_FILTER_BILINEAR);
+  UnloadImage(image);
+  return out;
+}
 
 std::string wrapping(std::string input, int linelength) {
   if (input.empty()) return input;
@@ -35,13 +51,15 @@ std::string wrapping(std::string input, int linelength) {
 
 int main() {
   InitWindow(w, h, title);
+  Texture2D test = resize("/home/alex/Downloads/images.jpg", 200, 200);
   SetTargetFPS(fps);
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText(
-      wrapping("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet est turpis, finibus tempus velit molestie at. Integer quis accumsan ante, a dictum tellus. ",50).c_str(),
-      50, 50, 20, BLACK);
+    // DrawText(
+    //   wrapping("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet est turpis, finibus tempus velit molestie at. Integer quis accumsan ante, a dictum tellus. ",50).c_str(),
+    //   50, 50, 20, BLACK);
+    DrawTexture(test, 50, 50, WHITE);
     EndDrawing();
   }
   CloseWindow();
